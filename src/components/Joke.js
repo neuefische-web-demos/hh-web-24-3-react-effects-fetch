f7ef60bef56ba89443d9b6aa428ac94f5d02456c
Jessica Lörs <jessica.loers@gmail.com>
2024-06-10 10:31:31 +0200

    initial commit

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,8 +1,45 @@
+import { useState, useEffect } from 'react';
+
 export default function Joke() {
+  const [joke, setJoke] = useState({});
+  const [jokeID, setJokeID] = useState(0);
+
+  console.log('THE JOKE_', joke);
+  console.log('THE JOKE ID_', jokeID);
+
+  // useEffect Hook:
+  // useEffect(() => {  }, [ ] )
+
+  useEffect(() => {
+    async function startFetching() {
+      try {
+        const response = await fetch(`https://example-apis.vercel.app/api/bad-okes/${jokeID}`);
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const fetchedJoke = await response.json();
+
+        setJoke(fetchedJoke);
+      } catch (error) {
+        console.error('The error:', error);
+      }
+    }
+
+    startFetching();
+  }, [jokeID]);
+
   return (
     <>
-      <h1>Insert joke here</h1>
-      <button type="button">Next Joke</button>
+      <h1>{joke.joke}</h1>
+      <button
+        type="button"
+        onClick={() => {
+          setJokeID(joke.nextId);
+        }}>
+        Next Joke
+      </button>
     </>
   );
 }
